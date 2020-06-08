6d455cbcbbe2d0165713a32a14af486db64c3232
smajidian <s_majidian@yahoo.com>
2020-06-08 16:58:16 +0430

    t

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='phase1',
+    version='v1.0dev',
+    packages=['phase1',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
