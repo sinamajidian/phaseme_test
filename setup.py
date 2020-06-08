303da6b6fe8ad5e3bb68f191a4e7f1b213148fd5
smajidian <s_majidian@yahoo.com>
2020-06-08 17:30:16 +0430

    Commit

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='phase1',
+    name='phaseme_test',
     version='v1.0dev',
-    packages=['phase1',],
+    packages=['phaseme_test',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
